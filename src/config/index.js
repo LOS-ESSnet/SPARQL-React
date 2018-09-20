f0a437c730efcf328b414d45c01c78664f345b87
Nicolas Laval <nicolas.laval@insee.fr>
2018-09-20 15:22:53 +0200

    Update endpoint configuration

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,12 @@
+const MAIN_ENDPOINT = 'http://SERVER/repositories';
+
 export default {
-	POP5_ENDPOINT: 'http://graphdb.linked-open-statistics.org/repositories/pop5',
-	PLOSH_ENDPOINT:
-		'http://graphdb.linked-open-statistics.org/repositories/plosh',
-	NUTS_ENDPOINT: 'http://graphdb.linked-open-statistics.org/repositories/nuts',
-	INSEE_ENDPOINT: 'http://rdf.insee.fr/sparql',
-	SIRENE_ENDPOINT: 'http://hackathon2018-2.ontotext.com/repositories/sirene',
-	CENSUS_POINT_ENDPOINT:
-		'http://hackathon2018-2.ontotext.com/repositories/census-point',
+	POP5_ENDPOINT: `${MAIN_ENDPOINT}/pop5`,
+	PLOSH_ENDPOINT: `${MAIN_ENDPOINT}/plosh`,
+	NUTS_ENDPOINT: `${MAIN_ENDPOINT}/nuts`,
+	INSEE_ENDPOINT: `http://rdf.insee.fr/sparql`,
+	SIRENE_ENDPOINT: `${MAIN_ENDPOINT}/geo-sirene`,
+	CENSUS_POINT_ENDPOINT: `${MAIN_ENDPOINT}/census-point`,
 };
 
 export const TOKEN = process.env.REACT_APP_MGL_TOKEN;
