113c8ace8322e7ab6b84e8b9f0f7597319d160c8
Nicolas Laval <nicolas.laval@insee.fr>
2018-09-20 15:25:51 +0200

    Add input component

---
+++ src/js/components/shared/input/input.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { TextField } from 'material-ui';
+
+function Input({ id, label, value, onChange, required, error, disabled, col }) {
+	const inputStyle = {
+		color: 'black',
+	};
+	const style = {
+		width: '90%',
+	};
+
+	const requiredText = value || !required ? '' : 'This field is required';
+	const errorText = error ? 'Error' : '';
+
+	return (
+		<div className={`mui-col-md-${col}`}>
+			<div className="mui-row">
+				<h2 className="mui-col-md-12">{label}</h2>
+			</div>
+			<div className="mui-row">
+				<TextField
+					className="mui-col-md-12"
+					id={id}
+					style={style}
+					inputStyle={inputStyle}
+					value={value}
+					onChange={e => onChange(e.target.value)}
+					disabled={disabled}
+					errorText={requiredText || errorText}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default Input;
