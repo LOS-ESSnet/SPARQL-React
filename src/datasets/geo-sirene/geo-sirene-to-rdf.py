0a95be13924ca39ba51f26e49e49dae62638a698
Nicolas Laval <nicolas.laval@insee.fr>
2018-10-02 10:22:38 +0200

    Update geo-sirene converter

--- src/datasets/geo-sirene/geo-sirene-to-rdf.py
+++ src/datasets/geo-sirene/geo-sirene-to-rdf.py
@@ -9,7 +9,7 @@ predicate_long = "geo-pos:long"
 predicate_lat = "geo-pos:lat"
 predicate_purpose = "org:purpose"
 predicate_located = "gn:locatedIn"
-predicate_eff = "mes:effectif"
+predicate_eff = "dim-etab:effectif"
 
 """Define csv headers"""
 SIREN = "SIREN"
@@ -33,40 +33,62 @@ BASE_URI_COM = "http://id.insee.fr/geo/commune/";
 def addChevrons( string ):
    return "<" + string + ">"
 
+def create_line( predicate, object ):
+   return "    " + predicate + " " + object.replace('"','')  + " ; \n"
+
 def create_line_string( predicate, object ):
    return "    " + predicate + " \"" + object.replace('"','')  + "\" ; \n"
 
-def create_line_url( predicate, object ):
+def create_line_uri( predicate, object ):
    return "    " + predicate + " " + addChevrons(object.replace('"',''))  + " ; \n"
 
 def create_line_float( predicate, object ):
    return "    " + predicate + " \"" + object.replace('"','')  + "\"^^xsd:float ; \n"
 
 def create_point(siret, lat, long):
-    point_url = addChevrons(BASE_URI_POINT+siret)
-    point ="    geo:hasPointGeometry %s .\n"%(point_url)
-    point += "%s geo:asWKT \"POINT(%s %s)\"^^geo:wktLiteral .\n"%(point_url, long, lat)
+    point_uri = addChevrons(BASE_URI_POINT+siret)
+    point ="    geo:hasPointGeometry %s .\n"%(point_uri)
+    point += "%s geo:asWKT \"POINT(%s %s)\"^^geo:wktLiteral .\n"%(point_uri, long, lat)
     return point
 
-def create_effectif_label(eff):
+def create_effectif_uri(eff):
     if (eff == 'NN'):
-        return "0 salarié ou effectif inconnu"
+        return "tranche-eff:0"
     eff = int(eff)
     if (eff >= 500):
-        return "500 salariés ou plus"
+        return "tranche-eff:500"
     if (eff >= 200):
-        return "200 à 499 salariés"
+        return "tranche-eff:200"
     if (eff >= 100):
-        return "100 à 199 salariés"
+        return "tranche-eff:100"
     if (eff >= 50):
-        return "50 à 99 salariés"
+        return "tranche-eff:50"
     if (eff >= 20):
-        return "10 à 49 salariés"
+        return "tranche-eff:20"
     if (eff >= 10):
-        return "10 à 49 salariés"
+        return "tranche-eff:10"
     if (eff >= 1):
-        return "1 à 9 salariés"
-    return "0 salarié ou effectif inconnu"
+        return "tranche-eff:1"
+    return "tranche-eff:0"
+
+def create_code_list_item(code, label):
+    item = "\n"
+    item += "tranche-eff:%s a skos:Concept ; \n"%(code)
+    item += "    skos:inScheme <http://id.insee.fr/codes/tranche-effectif> ; \n"
+    item += "    skos:prefLabel \"%s\"@fr . \n"%(label)
+    return item
+
+def create_code_list():
+   cl = "<http://id.insee.fr/codes/tranche-effectif> a skos:ConceptScheme . \n"
+   cl += create_code_list_item("0", "0 salarié ou effectif inconnu")
+   cl += create_code_list_item("1", "1 à 9 salariés")
+   cl += create_code_list_item("10", "10 à 19 salariés")
+   cl += create_code_list_item("20", "20 à 49 salariés")
+   cl += create_code_list_item("50", "50 à 99 salariés")
+   cl += create_code_list_item("100", "100 à 199 salariés")
+   cl += create_code_list_item("200", "200 à 499 salariés")
+   cl += create_code_list_item("500", "500 salariés ou plus")
+   return cl
 
 """Build ttl file from csv"""
 with open('geo-sirene.ttl', 'w', encoding="utf8") as output:
@@ -78,8 +100,15 @@ with open('geo-sirene.ttl', 'w', encoding="utf8") as output:
     output.write("@prefix  geo: <http://www.opengis.net/ont/geosparql#> .\n")
     output.write("@prefix  org: <http://www.w3.org/ns/org#> .\n")
     output.write("@prefix  gn: <http://www.geonames.org/ontology#> .\n")
-    output.write("@prefix  mes: <http://id.insee.fr/sirene/etablissement/> .\n")
+    output.write("@prefix  tranche-eff: <http://id.insee.fr/codes/tranche-effectif/> . \n")
+    output.write("@prefix  dim-etab: <http://id.insee.fr/sirene/etablissement/> .\n")
+    output.write("@prefix  skos: <http://www.w3.org/2004/02/skos/core#> . \n")
     output.write("\n")
+
+    """Write code list"""
+    output.write(create_code_list())
+    output.write("\n")
+
 
     with open('geo_sirene.csv', 'r', encoding="utf8") as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -108,12 +137,12 @@ with open('geo-sirene.ttl', 'w', encoding="utf8") as output:
             output.write(addChevrons(BASE_URI_SIRET+siret)+"\n")
             output.write("    rdf:type org:OrganizationalUnit ;\n ")
             output.write(create_line_string(predicate_name,row[indice_NOMEN_LONG]))
-            output.write(create_line_url(predicate_purpose,BASE_URI_API+better_apen))
-            output.write(create_line_url(predicate_located,BASE_URI_DEP+dep))
-            output.write(create_line_url(predicate_located,BASE_URI_COM+com))
+            output.write(create_line_uri(predicate_purpose,BASE_URI_API+better_apen))
+            output.write(create_line_uri(predicate_located,BASE_URI_DEP+dep))
+            output.write(create_line_uri(predicate_located,BASE_URI_COM+com))
             output.write(create_line_float(predicate_long,row[indice_LONGITUDE]))
             output.write(create_line_float(predicate_lat,row[indice_LATITUDE]))
-            output.write(create_line_string(predicate_eff,create_effectif_label(row[indice_EFF])))
+            output.write(create_line(predicate_eff,create_effectif_uri(row[indice_EFF])))
             output.write(create_point(siret, row[indice_LATITUDE], row[indice_LONGITUDE] ))
             output.write("\n")
 
